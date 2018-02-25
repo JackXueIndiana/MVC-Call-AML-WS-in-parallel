df248f942538be0a34998ebb6d0233a0fc6af78b
JackXueIndiana <33386334+JackXueIndiana@users.noreply.github.com>
2018-02-25 17:13:12 -0500

    Add files via upload

---
+++ Sample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSearchMVCApp
+{
+    public class Sample
+    {
+        public float soot { get; set; }
+        public float kv100 { get; set; }
+        public float aluminum { get; set; }
+        public float chromium { get; set; }
+        public float copper { get; set; }
+        public float iron { get; set; }
+        public float lead { get; set; }
+        public float tin { get; set; }
+        public float nickel { get; set; }
+        public float silicon { get; set; }
+        public float sodium { get; set; }
+        public float potassium { get; set; }
+        public float miles { get; set; }
+        public string fc { get; set; }
+        public int fci { get; set; }
+        public float scoreLable { get; set; }
+
+        public float probability { get; set; }
+    }
+}

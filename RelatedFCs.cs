1a65b4e3a5f51866db178a41a5df3e1285e17415
JackXueIndiana <33386334+JackXueIndiana@users.noreply.github.com>
2018-02-25 17:13:30 -0500

    Add files via upload

---
+++ RelatedFCs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSearchMVCApp
+{
+    public class RelatedFCs
+    {
+        public string fc { get; set; }
+        public string rfc1 { get; set; }
+        public string rfc2 { get; set; }
+        public string rfc3 { get; set; }
+        public string rfc4 { get; set; }
+        public string rfc5 { get; set; }
+    }
+}
